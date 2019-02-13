32580d3110df919e81c90e06c3607a0e3793bc8e
Krasimir Tsonev <krasimir@outset.ws>
2019-02-13 10:53:05 +0200

    Reverting some code snippet changes

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -1,10 +1,14 @@
 class HelloMessage extends React.Component {
   render() {
-    return <div>Привет, {this.props.name}</div>;
+    return (
+      <div>
+        Привет, { this.props.name }
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
   <HelloMessage name="Иван" />,
-  document.getElementById('hello-example'),
+  document.getElementById('hello-example')
 );
